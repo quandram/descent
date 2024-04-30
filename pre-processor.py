cf34d5d862c0f267ccb7cac38fb245877a04b88f
quandram <quandram@users.noreply.github.com>
2024-04-30 22:15:06 +0100

    Add header version - not refined

--- pre-processor.py
+++ pre-processor.py
@@ -9,16 +9,38 @@ target_file = './data/LONGMAN_1_OUTPUT.GED'
 shutil.copy(source_file, target_file)
 
 def pre_process_line(l):
+    appendToLine = "\n"
+    if (l == "0 HEAD"):
+        appendToLine = ""
+    elif (l == "1 SYST"):
+        appendToLine = ""
+        l = ""
+    elif (l == "2 SOUR Pedigree"):
+        appendToLine = ""
+        l = "\n1 SOUR Pedigree"
+        l = l + "\n2 VERS 2.4L"
+        l = l + "\n2 NAME Descent"
+    elif (l == "2 DEST PAF"):
+        appendToLine = ""
+        l = "\n1 DEST GRAMPS"
+        l = l + "\n1 SUBM Longman"
+        l = l + "\n1 GEDC"
+        l = l + "\n2 VERS 5.5.1"
+        l = l + "\n2 FORM LINEAGE-LINKED"
+        l = l + "\n1 CHAR UTF-8"
+
     if (l.startswith("0 @")):
-        # - @1@INDI -> @1@ INDI
-        # - @F1@FAM -> @F1@ FAM
-        return re.sub("0 @(.+?)@(\\S+?)", "0 @\\1@ \\2", l)
-    return l
+        # @1@INDI -> @1@ INDI
+        # @F1@FAM -> @F1@ FAM
+        l = re.sub("0 @(.+?)@(\\S+?)", "0 @\\1@ \\2", l)
+    # Remove I from individual record numbers: @I1@INDI -> @1@ INDI | HUSB @I78@ -> HUSB @78@
+    l = re.sub("@I(\\d+?)@", "@\\1@", l)
+    return appendToLine + l
 
 # some search and replace is required to make the file readable by the parser
 with open(target_file, "r+") as f:
-    ll = f.readlines()
+    lines = f.readlines()
     f.seek(0)
     f.truncate()
-    for l in ll:
-        f.write(pre_process_line(l))
+    for l in lines:
+        f.write(pre_process_line(l.rstrip()))
